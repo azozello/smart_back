3ebb7aee60d506404a7d3d3904f51196ac6cbead
Denis Panov <deonis.panoff@gmail.com>
2019-11-11 16:09:02 +0100

    Exception handling

--- app/main/controller/scrap_controller.py
+++ app/main/controller/scrap_controller.py
@@ -14,5 +14,24 @@ class TimeTable(Resource):
     @api.expect(_time_table, Validate=True)
     def post(self):
         data = request.json
-        scrap_time_table, login = create_scrappers(data['login'], data['password'])
-        return scrap_time_table()
+        try:
+            scrap_time_table, login = create_scrappers(data['login'], data['password'])
+            return scrap_time_table()
+        except Exception as e:
+            return [{
+                "day": "Error",
+                "lessons": [
+                    {
+                        "start": "07:30",
+                        "end": "09:00",
+                        "description": {
+                            "main": str(e),
+                            "additional": "",
+                            "teacher": "",
+                            "area": "",
+                            "type": "",
+                            "groups": ""
+                        }
+                    },
+                ]
+            }]
